61c58cc41c5673126fd27a2929d1db1cd792b5ae
Ryan Delaney <ryan.delaney@gmail.com>
2019-12-13 19:17:11 -0800

    Test argparse add_argument call

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,11 +1,23 @@
+import pytest as _pytest
 from downforeveryone import cli
 
 
-def test_argparser_called(mocker):
-    argparse_mock = mocker.patch("argparse.ArgumentParser", autospec=True)
-    cli.parse_args()
+@_pytest.fixture
+def argparse_mock(mocker):
+    return mocker.patch("argparse.ArgumentParser", autospec=True)
 
+
+def test_argparser_called(argparse_mock):
+    cli.parse_args()
     argparse_mock.assert_called_once_with(
         prog="isup",
         description="checks if a site is down for everyone or just you",
+    )
+
+
+def test_only_url_arg_added(argparse_mock):
+    cli.parse_args()
+
+    argparse_mock.return_value.add_argument.assert_called_once_with(
+        "url", help="url to test"
     )
