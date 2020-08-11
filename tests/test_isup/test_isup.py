585f09b0d0fb1abbfc0693bc91a31b608aff1e62
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2020-08-11 14:43:00 -0400

    Separate tests for query_url() function

--- tests/test_isup/test_isup.py
+++ tests/test_isup/test_isup.py
@@ -1,23 +1,6 @@
-import urllib.parse
-
 import pytest as _pytest
 
 from downforeveryone import isup
-
-
-class TestQueryUrl:
-    def test_raises_typerror(self):
-        with _pytest.raises(TypeError):
-            isup.query_url(0)  # type: ignore
-
-    def test_urljoin_called_once(self, mocker):
-        urllib_parse_mock = mocker.patch.object(
-            urllib.parse, "urljoin", autospec=True
-        )
-        isup.query_url("foo")
-        urllib_parse_mock.assert_called_once_with(
-            isup.__API_URL__["netloc"], isup.__API_URL__["path"] + "foo"
-        )
 
 
 class TestResponseHandler:
---
+++ tests/test_isup/test_query_url.py
@@ -0,0 +1,20 @@
+import urllib.parse
+
+import pytest as _pytest
+
+from downforeveryone import isup
+
+
+def test_raises_typerror():
+    with _pytest.raises(TypeError):
+        isup.query_url(0)  # type: ignore
+
+
+def test_urljoin_called_once(mocker):
+    urllib_parse_mock = mocker.patch.object(
+        urllib.parse, "urljoin", autospec=True
+    )
+    isup.query_url("foo")
+    urllib_parse_mock.assert_called_once_with(
+        isup.__API_URL__["netloc"], isup.__API_URL__["path"] + "foo"
+    )
