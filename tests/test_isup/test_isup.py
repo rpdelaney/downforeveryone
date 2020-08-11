364c4d8ac85d1f9c29331da74cbc8d7c700cf520
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2020-08-11 14:49:22 -0400

    Break out class since only main() tests are here now

--- tests/test_isup/test_isup.py
+++ tests/test_isup/test_isup.py
@@ -1,32 +1,32 @@
 from downforeveryone import isup
 
 
-class TestMain:
-    def test_args_parsed(self, cli_args_mock, isitup_mock, sys_exit_mock):
-        isup.main()
+def test_args_parsed(cli_args_mock, isitup_mock, sys_exit_mock):
+    isup.main()
 
-        cli_args_mock.assert_called_once_with()
+    cli_args_mock.assert_called_once_with()
 
-    def test_exit_code(self, cli_args_mock, isitup_mock, sys_exit_mock):
-        isup.main()
 
-        sys_exit_mock.assert_called_once_with(isitup_mock.return_value)
+def test_exit_code(cli_args_mock, isitup_mock, sys_exit_mock):
+    isup.main()
 
-    def test_exception_exits_3(
-        self, cli_args_mock, isitup_mock, sys_exit_mock
-    ):
-        isitup_mock.side_effect = Exception
+    sys_exit_mock.assert_called_once_with(isitup_mock.return_value)
 
-        isup.main()
 
-        sys_exit_mock.assert_called_once_with(3)
+def test_exception_exits_3(cli_args_mock, isitup_mock, sys_exit_mock):
+    isitup_mock.side_effect = Exception
 
-    def test_exception_prints_traceback(
-        self, cli_args_mock, isitup_mock, sys_exit_mock, mocker
-    ):
-        isitup_mock.side_effect = Exception
-        traceback_mock = mocker.patch("traceback.print_exc")
+    isup.main()
 
-        isup.main()
+    sys_exit_mock.assert_called_once_with(3)
 
-        traceback_mock.assert_called_once_with()
+
+def test_exception_prints_traceback(
+    cli_args_mock, isitup_mock, sys_exit_mock, mocker
+):
+    isitup_mock.side_effect = Exception
+    traceback_mock = mocker.patch("traceback.print_exc")
+
+    isup.main()
+
+    traceback_mock.assert_called_once_with()
