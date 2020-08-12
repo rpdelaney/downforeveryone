8ee68619f5056aedca17c979b9292aef68cd6a5a
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2020-08-12 17:30:57 -0400

    Remove deprecated fixtures

--- tests/test_isup/conftest.py
+++ tests/test_isup/conftest.py
@@ -1,5 +1,3 @@
-from json.decoder import JSONDecodeError
-
 import pytest as _pytest
 import responses
 
@@ -15,35 +13,6 @@ def fake_response_args(url="https://foo.bar", status=200):
         "status": status,
         "content_type": "application/json",
     }
-
-
-@_pytest.fixture
-def mock_request_success(mocker):
-    fake = mocker.patch("requests.models.Response", autospec=True)
-    fake.ok = True
-    fake.status_code = 200
-    return fake
-
-
-@_pytest.fixture
-def mock_request_failure(mocker):
-    # This test does not mock the response object as intended, which
-    # is causing test coverage to fall below the minimum as the JSONDecodeError
-    # is not actually being raised.
-    # TODO: Look into https://github.com/getsentry/responses for mocking
-    # requests response objects
-    fake = mocker.patch("requests.models.Response", autospec=False)
-    fake.ok = False
-    fake.status_code = 404
-    fake.json.return_value = mocker.Mock(side_effect=JSONDecodeError)
-    return fake
-
-
-@_pytest.fixture
-def requests_get_mock(mocker, mock_request_success):
-    mock_response = mocker.patch("requests.get", autospec=True)
-    mock_response.return_value = mock_request_success
-    return mock_response
 
 
 @_pytest.fixture
