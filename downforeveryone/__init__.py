259d23d42bcf076bef08bacbd0d0de477bddc851
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2021-12-03 15:52:10 -0500

    Add logic for version inference from pyproject

--- downforeveryone/__init__.py
+++ downforeveryone/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "1.0.1"
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "UNKNOWN"
