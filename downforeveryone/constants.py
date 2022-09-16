0225a9bc62e87a4da3f0e87976245e7df8609a57
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2022-09-16 15:06:45 -0400

    Fix API endpoint URL

--- downforeveryone/constants.py
+++ downforeveryone/constants.py
@@ -2,12 +2,12 @@ from types import MappingProxyType
 
 from downforeveryone import useragents
 
-API_URL = "https://api-prod.downfor.cloud/httpcheck/{domain}"
+API_URL = "https://downforeveryoneorjustme.com/api/httpcheck/{domain}"
 
 QUERY_HEADERS = MappingProxyType(
     {
         "User-Agent": useragents.random_agent(),
-        "Referer": "https://api.downfor.cloud/",
+        "Referer": "https://downforeveryoneorjustme.com",
         "Origin": "https://downforeveryoneorjustme.com",
         "Accept": "*/*",
         "Accept-Language": "en-US,en;q=0.5",
