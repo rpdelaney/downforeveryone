712c0c74dd3dfa53f4e7106361d4333e2fb7dbd5
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2023-08-27 14:40:40 -0400

    Add some docstrings for tests

--- tests/test_cli/test_version.py
+++ tests/test_cli/test_version.py
@@ -8,15 +8,18 @@ import downforeveryone
 
 
 def test_version_set():
+    """Verify that there is a version."""
     assert isinstance(downforeveryone.__version__, str)
     assert len(downforeveryone.__version__) > 0
 
 
 def test_version_is_semver():
+    """Verify that the version conforms to the semver spec."""
     assert semver.VersionInfo.parse(downforeveryone.__version__)
 
 
 def test_version_matches_pyproject():
+    """Verify that the version matches the build system."""
     root_dir = pathlib.Path(__file__).parent.parent.parent
     with open(os.path.join(root_dir, "pyproject.toml")) as f:
         pyproject = toml.load(f)
